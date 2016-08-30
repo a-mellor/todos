8d358f35c50f89ea8266a9befb1b05cc0346722d
alex <alexander.mellor@gmail.com>
2016-08-30 16:39:54 +0100

    Ensure todos are displayed for the corrent user

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -5,7 +5,9 @@ require File.expand_path('../../config/environment', __FILE__)
 abort("The Rails environment is running in production mode!") if Rails.env.production?
 require 'spec_helper'
 require 'rspec/rails'
-require 'support/features/sign_in'  
+require 'support/features/sign_in'
+require 'support/database_cleaner'
+
 # Add additional requires below this line. Rails is not loaded until this point!
 
 # Requires supporting ruby files with custom matchers and macros, etc, in
